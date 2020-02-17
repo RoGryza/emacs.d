0848e3d998bdc76b382360919dd7ff2475bdcd19
Rodrigo Gryzinski <rogryza@gmail.com>
2020-02-16 21:21:12 -0300

    Delete dead code

--- config/my-projectile.el
+++ config/my-projectile.el
@@ -3,33 +3,6 @@
 (defvar my-leader)
 (defvar my-projectile-project-manager-fn nil)
 
-;; (cl-defun my-projectile-register-project-type
-;;     (project-type marker-files
-;;                   &key compilation-dir configure compile test run test-suffix test-prefix src-dir test-dir related-files-fn manager-fn)
-;;   (projectile-register-project-type project-type marker-files
-;;                                     :compilation-dir compilation-dir
-;;                                     :configure configure
-;;                                     :compile compile
-;;                                     :test test
-;;                                     :run run
-;;                                     :test-suffix test-suffix
-;;                                     :test-prefix test-prefix
-;;                                     :src-dir src-dir
-;;                                     :test-dir test-dir
-;;                                     :related-files-fn related-files-fn)
-;;   (let ((project-plist (alist-get project-type projectile-project-types)))
-;;     (when manager-fn
-;;       (plist-put project-plist 'manager-fn manager-fn))
-;;     project-plist))
-
-;; (defun my-projectile-manager-fn (project-type)
-;;   (or my-projectile-project-manager-fn
-;;       (plist-get (alist-get project-type projectile-project-types) 'manager-fn)))
-
-;; (defun my-projectile-manager-project (arg)
-;;   (interactive "P")
-;;   (funcall (my-projectile-manager-fn (projectile-project-type))))
-
 (use-package projectile
   :ensure projectile
   :general
